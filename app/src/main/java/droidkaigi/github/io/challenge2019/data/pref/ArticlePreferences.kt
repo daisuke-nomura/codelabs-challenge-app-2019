376561f042541fb13be5569c7a9811b9a618233b
Daisuke Nomura <daisuke.nomula@gmail.com>
2019-02-11 11:12:45 +0900

    refactoring preference

--- app/src/main/java/droidkaigi/github/io/challenge2019/data/pref/ArticlePreferences.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/data/pref/ArticlePreferences.kt
@@ -1,26 +1,40 @@
 package droidkaigi.github.io.challenge2019.data.pref
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.preference.PreferenceManager
+import androidx.core.content.edit
 
 class ArticlePreferences {
 
     companion object {
-        private const val ARTICLE_IDS_KEY = "article_ids_key"
+
+        private lateinit var p: SharedPreferences
 
         fun saveArticleIds(context: Context, articleId: String) {
-            val p = PreferenceManager.getDefaultSharedPreferences(context)
-            val data = p.getStringSet(ARTICLE_IDS_KEY, mutableSetOf()) ?: mutableSetOf()
-            val tmps = mutableSetOf<String>()
-            tmps.addAll(data)
-            tmps.add(articleId)
-            p.edit().putStringSet(ARTICLE_IDS_KEY, tmps).commit()
+            if (!this::p.isInitialized) {
+                p = PreferenceManager.getDefaultSharedPreferences(context)
+            }
+
+            val data = p.getStringSet(ARTICLE_IDS_KEY, mutableSetOf())!!
+            val tmps = mutableSetOf<String>().apply {
+                this.addAll(data)
+                this.add(articleId)
+            }
+            p.edit {
+                putStringSet(ARTICLE_IDS_KEY, tmps)
+            }
         }
 
         fun getArticleIds(context: Context): Set<String> {
-            val p = PreferenceManager.getDefaultSharedPreferences(context)
+            if (!this::p.isInitialized) {
+                p = PreferenceManager.getDefaultSharedPreferences(context)
+            }
+
             return p.getStringSet(ARTICLE_IDS_KEY, setOf()) ?: setOf()
         }
     }
 
 }
+
+private const val ARTICLE_IDS_KEY = "article_ids_key"
